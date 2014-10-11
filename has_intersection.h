8bd2e8d73041d456b4541a92c4027aad0c77d80f
Ilya Isaev <isaev.ilya.1994@gmail.com>
2014-10-11 18:09:14 +0400

    minor

--- has_intersection.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "segment.h"
-bool has_intersection(Segment seg1, Segment seg2);
-Point<double> get_euclide_coords(double r, double polar_angle, double azimuth);
--- segment.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "point.h"
-struct Segment{
-    Segment(Point<double> &left, Point<double> &right) : left(left), right(right) {};
-    ~Segment();
-    Point<double>& left;
-    Point<double>& right;
-};
